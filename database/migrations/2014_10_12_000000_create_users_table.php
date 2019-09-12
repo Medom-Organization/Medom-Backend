5bd47528364872f8179c44cd836d6ddcd1a3c4e3
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-09-12 13:01:54 -0700

    updated user migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('id');
+            $table->primary('id');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('role_id');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number')->nullable();
+            $table->string('role');
             $table->timestamps();
         });
     }
