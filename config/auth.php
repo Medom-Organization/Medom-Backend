f01d9120eaad5b09a64e29f84cf24bb94e6dee58
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-09-12 21:28:57 -0700

    fixed local issue

--- config/auth.php
+++ config/auth.php
@@ -13,8 +13,12 @@ return [
     |
     */
 
+    // 'defaults' => [
+    //     'guard' => 'web',
+    //     'passwords' => 'users',
+    // ],
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -42,9 +46,8 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
