8ffa534411d23038ba5106e35805516e8de2024e
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-09-14 13:00:19 -0700

    updated hospitals.php

--- modules/Auth/Api/v1/Controllers/AuthController.php
+++ modules/Auth/Api/v1/Controllers/AuthController.php
@@ -15,11 +15,10 @@ use Medom\Modules\Auth\Api\v1\Transformers\RoleTransformer;
 class AuthController extends BaseController
 {
 
-    public function __construct(AuthRepository $authRepo,UserTransformer $userTransformer)
+    public function __construct(AuthRepository $authRepo, UserTransformer $userTransformer)
     {
         $this->authRepo = $authRepo;
         $this->userTransformer = $userTransformer;
-
     }
 
     public function login(LoginRequest $request)
@@ -27,36 +26,36 @@ class AuthController extends BaseController
         $credentials = $request->only('email', 'password');
         // $credentials = request(['email', 'password']);
 
-        if (! $token = auth()->attempt($credentials)) {
+        if (!$token = auth()->attempt($credentials)) {
             return $this->fail("Invalid login credentials");
         }
 
+        // if
         $data = [
-            'token'=>$token,
-            'user'=>auth()->user()
+            'token' => $token,
+            'user' => auth()->user()
         ];
 
         return $this->success($data);
-
     }
 
     public function registerUser(RegistrationRequest $request)
     {
-        $user =$this->authRepo->createUser($request);
+        $user = $this->authRepo->createUser($request);
     }
     public function registerHospital(HospitalRegistrationRequest $request)
     {
-        $user =$this->authRepo->createHospital($request);
+        $user = $this->authRepo->createHospital($request);
     }
 
-    public function updateProfile(UpdateProfileRequest $request){
+    public function updateProfile(UpdateProfileRequest $request)
+    {
 
         $user = $this->authRepo->updateAccount($request->all());
 
-        if($user)
-            return $this->transform($user,$this->userTransformer);
+        if ($user)
+            return $this->transform($user, $this->userTransformer);
 
         return $this->error("Unable to update user profile");
     }
-
 }
