5ab8cbbe93a7974b74663a158042b294ebfd68a2
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-10-17 06:17:55 -0700

    add method to get users

--- modules/Hospitals/Api/v1/Repositories/HospitalRepository.php
+++ modules/Hospitals/Api/v1/Repositories/HospitalRepository.php
@@ -27,56 +27,58 @@ class HospitalRepository extends BaseRepository
         $this->hospitalStaffModel = new HospitalStaff;
         $this->settingsHospitalModel = new Settingshospital;
         $this->hospitalAdminModel = new HospitalAdmin;
-        $this->hospitalProfessional= new HospitalProfessional;
-        $this->Professional= new Professional;
-
+        $this->hospitalProfessional = new HospitalProfessional;
+        $this->Professional = new Professional;
     }
 
-  
-    public function getHospitalProfessional(Type $var = null)
+
+    public function getHospitalProfessional()
     {
-        $result=$this->hospitalProfessional->all();
-        return $result;
+        $result = $this->hospitalProfessional->all();
+        foreach ($result as $result) {
+            $data[] =$this->userModel->where('id', $result->id);
+        }
+        return $data;
     }
     public function addHospitalProfessional($request, $id)
     {
-        $user=auth()->user();
-        $validatehospitaladmin=$this->hospitalAdminModel->where('user_id', $user->id)->get();
-        if($validatehospitaladmin){
-            $hospital_id=$this->hospitalModel->where('name', $request['hospitalname'])->get();
-        }else{
-            return response()->json(['status'=>false, 'message'=>'Access Denied'], 401);
+        $user = auth()->user();
+        $validatehospitaladmin = $this->hospitalAdminModel->where('user_id', $user->id)->get();
+        if ($validatehospitaladmin) {
+            $hospital_id = $this->hospitalModel->where('name', $request['hospitalname'])->get();
+        } else {
+            return response()->json(['status' => false, 'message' => 'Access Denied'], 401);
         }
-        $check=$this->Professional->where('id', $id)->get();
-        $role=$this->roleModel->where('name', 'hospitalprofessional')->get();
-        if($check){
-        $professional=$this->Professional->where('id', $id)->update([
-            'role_id'=>$role->id,
-            'status'=>'verified',
-        ]);
-        $hospitalProfessional=$this->hospitalProfessional->create([
-            'user_id'=>$id,
-            'hospital_id'=>$hospital_id->id,
-            'role_id'=>$role->id,
-        ]);
-        }else{
-            return response()->json(['status'=>false, 'message'=>'professional not found'], 500);
+        $check = $this->Professional->where('id', $id)->get();
+        $role = $this->roleModel->where('name', 'hospitalprofessional')->get();
+        if ($check) {
+            $professional = $this->Professional->where('id', $id)->update([
+                'role_id' => $role->id,
+                'status' => 'verified',
+            ]);
+            $hospitalProfessional = $this->hospitalProfessional->create([
+                'user_id' => $id,
+                'hospital_id' => $hospital_id->id,
+                'role_id' => $role->id,
+            ]);
+        } else {
+            return response()->json(['status' => false, 'message' => 'professional not found'], 500);
         }
 
         // ret
     }
     public function registerProfessional()
     {
-        $user=auth()->user();
-        $role=$this->roleModel->where('name', 'unverifiedprofessional')->get();
+        $user = auth()->user();
+        $role = $this->roleModel->where('name', 'unverifiedprofessional')->get();
         $this->userModel->where('id', $user->id)->update([
-            'role_id'=>$role->id,
-            'role'=>$role->name
+            'role_id' => $role->id,
+            'role' => $role->name
         ]);
-        $professional=$this->Professional->updateorcreate([
-            'user_id'=>$user->id,
-            'role_id'=>$role->id,
-            'status'=>'unverified'
+        $professional = $this->Professional->updateorcreate([
+            'user_id' => $user->id,
+            'role_id' => $role->id,
+            'status' => 'unverified'
         ]);
     }
     public function getAllProfessionals()
