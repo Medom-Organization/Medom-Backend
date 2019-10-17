60d3679a14658faad7a76c5f1e3aed9aed9f4c79
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-10-17 05:43:09 -0700

    adding parameters

--- modules/Hospitals/Api/v1/Controllers/HospitalController.php
+++ modules/Hospitals/Api/v1/Controllers/HospitalController.php
@@ -7,6 +7,7 @@ use Medom\Modules\BaseController;
 use Medom\Modules\Hospitals\Api\v1\Repositories\HospitalRepository;
 use Illuminate\Http\Request;
 use Medom\Modules\Hospitals\Api\v1\Requests\AddHospitalProfessionalRequest;
+use Medom\Modules\Hospitals\Api\v1\Requests\RegisterProfessionalRequest;
 use Medom\Modules\Hospitals\Api\v1\Requests\AddHospitalStaffRequest;
 // use Medom\Modules\Hospitals\Api\v1\Requests\HospitalRegistrationRequest;
 use Medom\Modules\Hospitals\Api\v1\Transformers\UserTransformer;
@@ -21,11 +22,11 @@ class HospitalController extends BaseController
     }
     public function addHospitalAdmin(Type $var = null)
     {
-        $result=$this->hospitalRepo->addHospitalAdmin();
+        $result = $this->hospitalRepo->addHospitalAdmin();
     }
-    public function addHospitalProfessional(Type $request,$id)
+    public function addHospitalProfessional(Type $request, $id)
     {
-        $result=$this->hospital->addHospitalProfessional($request->all(), $id);
+        $result = $this->hospital->addHospitalProfessional($request->all(), $id);
         // retur
     }
     public function addHospitalstaff(Type $var = null)
@@ -34,16 +35,16 @@ class HospitalController extends BaseController
     }
     public function getHospitalProfessional(Type $var = null)
     {
-        $result=$this->hospitalRepo->getHospitalProfessional();
+        $result = $this->hospitalRepo->getHospitalProfessional();
     }
     public function registerProfessional()
     {
-        $result=$this->hospitalRepo->registerProfessional();
+        $result = $this->hospitalRepo->registerProfessional();
         return $result;
     }
     public function getAllProfessionals()
     {
-        $result=$this->hospitalRepo->getAllProfessional();
+        $result = $this->hospitalRepo->getAllProfessional();
         return $result;
     }
 }
