b5e84c02f91b5e2f1f73ee545e6fe885b33b763e
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-10-26 20:05:07 -0700

    added orderformrequest

---
+++ modules/Bookings/Api/v1/Requests/NewOrderRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Medom\Modules\Booking\Api\v1\Requests;
+
+use Dingo\Api\Http\FormRequest;
+
+
+class NewOrderRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'price_info' => 'required',
+            'booking_reference' => 'required',
+            'ticket_time_limit' => 'required',
+            'total_travellers' => 'required',
+            'traveller_stats' => 'required',
+            'travellers' => 'required',
+            'from' => 'required',
+            'to' => 'required',
+            'departure_city' => 'required',
+            'destination_city' => 'required',
+            'flight_info' => 'required',
+            'departure_date' => 'required',
+            'return_date' => 'required'
+        ];
+    }
+}
