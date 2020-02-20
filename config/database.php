45cbc211c22acb812ea43cdc850f51c83a2e7ee9
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2020-02-20 12:57:04 -0800

    edited config for database

--- config/database.php
+++ config/database.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Str;
 
-// $url = parse_url(getenv("DATABASE_URL"));
+$url = parse_url(getenv("DATABASE_URL"));
 
-// $host = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $database = substr($url["path"], 1);
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -99,18 +99,18 @@ return [
         //     'schema' => 'public',
         //     'sslmode' => 'prefer',
         // ],
-        // 'pgsql' => [
-        //     'driver'   => 'pgsql',
-        //     'host'     => $host,
-        //     'database' => $database,
-        //     'username' => $username,
-        //     'password' => $password,
-        //     'charset'  => 'utf8',
-        //     'prefix'   => '',
-        //     'prefix_indexes' => true,
-        //     'schema' => 'public',
-        //     'sslmode' => 'prefer',
-        // ],
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
