7207e38100b49dcfaa8b0ab8051074c60e341a42
Daniel <mabadejedaniel1@gmail.com>
2020-02-27 22:52:43 -0800

    edited welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,12 +88,11 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://medom.netlify.com/">Docs</a>
+                    <a href="https://medom.netlify.com/">Medom</a>
+                    <a href="https://medom.netlify.com/">News</a>
+                    <a href="https://medom.netlify.com/">Blog</a>
+                    <a href="https://medom.netlify.com/">About</a>
                     <a href="https://github.com/medom-organization">GitHub</a>
                 </div>
             </div>
